9e4bf3e3222c00c93dcb44052e5350b5108fd37c
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-09-05 18:31:05 -0500

    first commit

---
+++ authProject/authApp/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+#for class based views
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+# Import the user class (model)
+from django.contrib.auth.models import User
+
+#import register form
+from .forms import RegisterForm
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = User.objects.create_user(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    error_mgs = None
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            next_url = request.POST.get('next') or request.GET.get('next') or 'home'
+            return redirect(next_url)
+        else:
+            error_mgs = "Invalid Credentials!"
+    
+    return render(request, 'accounts/login.html', {'error': error_mgs})
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('login')
+    else:
+        return redirect('home')
+    
+
+#Home view
+# Using the decorators
+@login_required
+def home_view(request):
+    return render(request, 'authApp/home.html')
+
+#Protected View
+class ProtectedView(LoginRequiredMixin, View):
+    login_url = '/login/'
+    # 'next' - to redirect URL
+    redirect_field_name = 'redirect_to'
+
+    def get(self, request):
+        return render(request, 'registration/protected.html')
+
+
+
+
+
+
